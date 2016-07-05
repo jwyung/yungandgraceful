47e05e5e57d7d7391e30faec6d3787d8c5395d24
Jonathan Yung <jyung@slack-corp.com>
2016-07-04 21:21:39 -0700

    on rsvp form success callback, we show animation: form -> spinner -> confirmation message

--- js/core.js
+++ js/core.js
@@ -2,13 +2,17 @@
 	var $rsvpForm = $('.rsvp-form');
 
 	$rsvpForm.on('submit', function(e) {
+		$rsvpForm.addClass('submitted');
+		var $spinner = $('.spinner .preloader-wrapper').addClass('active show');
+
 		$.ajax({
 			url: atob('aHR0cHM6Ly9mb3Jtc3ByZWUuaW8veXVuZ2FuZGdyYWNlZnVsQGdtYWlsLmNvbQ=='),
 			method: 'POST',
 			data: $(this).serialize(),
 			dataType: 'json',
 			success: function() {
-				console.log('success!');
+				$spinner.removeClass('show');
+				$('.confirmation').addClass('show');
 			}
 		});
 
