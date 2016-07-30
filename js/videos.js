9eed61d03adb00782e3534bd15d8cb4675282c48
Jonathan Yung <jyung@slack-corp.com>
2016-07-30 16:02:12 -0700

    adds a sample video. also, we define the user experience for scrolling to, playing, and pausing an example video

---
+++ js/videos.js
@@ -0,0 +1,99 @@
+(function($) {
+	var $examples = $('.example');
+	var scrollFireSettings = {
+		offset: $examples.innerHeight() * 3/4,
+		timeout: {}
+	};
+
+	var options = [
+		{
+			selector: '.gaming-video',
+			offset: scrollFireSettings.offset,
+			callback: function(el) {
+				videoScrollFireHandler($examples.find('.gaming-video')[0]);
+			}
+		},
+		{
+			selector: '.weirdness-video',
+			offset: scrollFireSettings.offset,
+			callback: function(el) {
+				videoScrollFireHandler($examples.find('.weirdness-video')[0]);
+			}
+		},
+		{
+			selector: '.road-trip-video',
+			offset: scrollFireSettings.offset,
+			callback: function(el) {
+				videoScrollFireHandler($examples.find('.road-trip-video')[0]);
+			}
+		}
+	];
+
+	function videoScrollFireHandler(video) {
+		playVideo(video);
+		video.playbackRate = 0.5;
+		video.setAttribute('muted', true);
+
+		scrollFireSettings.timeout[video.className] = setTimeout(function() {
+			pauseVideo(video);
+			video.playbackRate = 1;
+			video.setAttribute('muted', false);
+		}, 2000);
+	}
+
+	Materialize.scrollFire(options);
+
+
+	$('.example')
+		.on('click', '.play-btn', play)
+		.on('click', '.example-video', pause)
+		.on('mouseover', '.example-video', showControls)
+		.on('mousedown', '.example-video', hideControls);
+
+	function play() {
+		var $example = $(this).closest('.example');
+		var video = $example.find('.example-video')[0];
+
+		clearTimeout(scrollFireSettings.timeout[video.className]);
+
+		$.map($examples, function(example) {
+			if ($(example).hasClass('playing')) {
+				pause.call(example);
+			}
+		});
+
+		$example.addClass('playing');
+		playVideo(video);
+	}
+
+	function pause() {
+		var $example = $(this).closest('.example');
+		var video = $example.find('.example-video')[0];
+
+		$example.removeClass('playing');
+		pauseVideo(video);
+		video.removeAttribute('controls');
+	}
+
+	function showControls() {
+		if ($(this).closest('.playing').length) {
+			this.setAttribute('controls', 'controls');
+		}
+	}
+
+	function hideControls() {
+		this.removeAttribute('controls');
+	}
+
+	function playVideo(video) {
+		video.play();
+		video.isPaused = false;
+		video.playbackRate = 1;
+		video.setAttribute('muted', false);
+	}
+
+	function pauseVideo(video) {
+		video.pause();
+		video.isPaused = true;
+	}
+}(jQuery));
