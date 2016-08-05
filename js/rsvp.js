bf93d4c35bd24117940e0a60be8292ed12c0a71d
Jonathan Yung <jyung@slack-corp.com>
2016-08-04 20:56:33 -0700

    change the look and feel for RSVP form section. Introducing a cleaner design for desktop and better adjacent color scheme. Makes the first guest form enable and disable buttons accordingly

--- js/rsvp.js
+++ js/rsvp.js
@@ -4,13 +4,16 @@
 	var $g1FirstName = $('#g1-first-name');
 	var $g1LastName = $('#g1-last-name');
 	var $g1Item = $('.g1-item');
+	var $g1GuestItem = $('.g1 .guest-item');
 
 	var $g2FirstName = $('#g2-first-name');
 	var $g2LastName = $('#g2-last-name');
 	var $g2Item = $('.g2-item');
 
+
 	$rsvpForm.on('keyup', '#g1-first-name, #g1-last-name', function(e) {
 		$g1Item.attr('disabled', !( $g1FirstName.val() && $g1LastName.val() ));
+		maybeEnableSubmitBtn($rsvpForm.find('.g1'));
 	});
 
 	$rsvpForm.on('keyup', '#g2-first-name, #g2-last-name', function(e) {
@@ -18,7 +21,19 @@
 		$g2Item.attr('required', $g2FirstName.val() && $g2LastName.val() );
 	});
 
+	$rsvpForm.find('.g1').on('change', '[name=g1-rsvp], [name=g1-entree]', function(e) {
+		maybeEnableSubmitBtn($rsvpForm.find('.g1'));
+	});
+
+	$rsvpForm.on('click', '.plus-one-btn', function(e) {
+		$(this).addClass('hidden');
+		$rsvpForm.find('.g2').addClass('show');
+	});
+
 	$rsvpForm.on('submit', function(e) {
+		// TODO: this is not ready yet. Let's not do anything
+		return false;
+
 		$rsvpForm.addClass('submitted');
 		var $spinner = $('.spinner .preloader-wrapper').addClass('active show');
 
@@ -35,4 +50,24 @@
 
 		return false;
 	});
+
+	function maybeEnableSubmitBtn($guestFieldset) {
+		$rsvpForm.find('.rsvp-btn').attr('disabled', !(isNameFilled($guestFieldset) && isAttendanceFilled($guestFieldset) && isEntreeFilled($guestFieldset)));
+	}
+
+	function isNameFilled($guestFieldset) {
+		return $guestFieldset.find('.first-name-field').val() && $guestFieldset.find('.last-name-field').val();
+	}
+
+	function isAttendanceFilled($guestFieldset) {
+		var guest = $guestFieldset.attr('class').split(' ')[0];
+
+		return !!$guestFieldset.find('[name=' + guest + '-rsvp]:checked').val();
+	}
+
+	function isEntreeFilled($guestFieldset) {
+		var guest = $guestFieldset.attr('class').split(' ')[0];
+
+		return !!$guestFieldset.find('[name=' + guest + '-entree]:checked').val();
+	}
 }(jQuery));
