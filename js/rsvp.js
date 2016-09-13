2047b76ffc04ea58ea997a5f8495edd780dcbc08
Jonathan Yung <jyung@slack-corp.com>
2016-09-12 23:09:37 -0700

    created error states for RSVP form and made sure buttons are disabled when it makes sense (i.e. if the user did not enter both first and last name, then attendance buttons should be disabled. also, if the user selected not attending, entree buttons should be disabled.)

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,33 +1,72 @@
 (function($) {
 	var $rsvpForm = $('.rsvp-form');
-
-	var $g1FirstName = $('#g1-first-name');
-	var $g1LastName = $('#g1-last-name');
 	var $g1Item = $('.g1-item');
-	var $g1GuestItem = $('.g1 .guest-item');
-
-	var $g2FirstName = $('#g2-first-name');
-	var $g2LastName = $('#g2-last-name');
 	var $g2Item = $('.g2-item');
 
+	$rsvpForm.on('keyup', '.input-text', function(e) {
+		var $guestFieldset = getGuestFieldset.call(this);
+		var guest = getGuest($guestFieldset);
+		var nameFilled = isNameFilled($guestFieldset);
 
-	$rsvpForm.on('keyup', '#g1-first-name, #g1-last-name', function(e) {
-		$g1Item.attr('disabled', !( $g1FirstName.val() && $g1LastName.val() ));
-		maybeEnableSubmitBtn($rsvpForm.find('.g1'));
+		if (nameFilled) {
+			$guestFieldset.find('input[name=' + guest + '-rsvp]').attr('disabled', false);
+
+			$guestFieldset.find('.rsvp-content').removeClass('show-error');
+		} else {
+			$guestFieldset.find('input[name=' + guest + '-rsvp]').attr('disabled', true);
+			$guestFieldset.find('input[name=' + guest + '-rsvp]').attr('checked', false);
+
+			$guestFieldset.find('input[name=' + guest + '-entree]').attr('disabled', true);
+			$guestFieldset.find('input[name=g1-entree]').attr('checked', false);
+		}
+
+		maybeEnableSubmitBtn($guestFieldset);
 	});
 
-	$rsvpForm.on('keyup', '#g2-first-name, #g2-last-name', function(e) {
-		$g2Item.attr('disabled', !( $g2FirstName.val() && $g2LastName.val() ));
-		$g2Item.attr('required', $g2FirstName.val() && $g2LastName.val() );
+	$rsvpForm.on('change', '.guest-item-radio', function(e) {
+		maybeEnableSubmitBtn(getGuestFieldset.call(this));
 	});
 
-	$rsvpForm.find('.g1').on('change', '[name=g1-rsvp], [name=g1-entree]', function(e) {
-		maybeEnableSubmitBtn($rsvpForm.find('.g1'));
+	$rsvpForm.on('change', '.item-rsvp', function(e) {
+		var $guestFieldset = getGuestFieldset.call(this);
+		var $entrees = $guestFieldset.find('.item-entree');
+
+		if ($guestFieldset.find('.item-rsvp:checked').val() === 'yes') {
+			$guestFieldset.find('.entree-content').removeClass('show-error');
+			$entrees.attr('disabled', false);
+		} else {
+			$entrees.attr('disabled', true);
+			$entrees.attr('checked', false);
+		}
 	});
 
 	$rsvpForm.on('click', '.plus-one-btn', function(e) {
 		$(this).addClass('hidden');
 		$rsvpForm.find('.g2').addClass('show');
+	});
+
+	$rsvpForm.on('click', '.item-rsvp-label', function(e) {
+		var $guestFieldset = getGuestFieldset.call(this);
+
+		if (!isNameFilled($guestFieldset)) {
+			$(this).closest('.guest-item-content').addClass('show-error');
+		}
+	});
+
+	$rsvpForm.on('click', '.item-entree-label', function(e) {
+		var $guestFieldset = getGuestFieldset.call(this);
+
+		if (isAttendanceFilled($guestFieldset)) {
+			$guestFieldset.find('.rsvp-not-selected').addClass('hidden');
+			$guestFieldset.find('.rsvp-selected').removeClass('hidden');
+		} else {
+			$guestFieldset.find('.rsvp-not-selected').removeClass('hidden');
+			$guestFieldset.find('.rsvp-selected').addClass('hidden');
+		}
+
+		if (!isAttending($guestFieldset)) {
+			$(this).closest('.guest-item-content').addClass('show-error');
+		}
 	});
 
 	$rsvpForm.on('submit', function(e) {
@@ -51,8 +90,23 @@
 		return false;
 	});
 
+	function getGuestFieldset() {
+		return $(this).closest('.guest');
+	}
+
+	function getGuest($guestFieldset) {
+		var classnames = $guestFieldset.attr('class');
+
+		return classnames && classnames.split(' ')[0];		
+	}
+
+
 	function maybeEnableSubmitBtn($guestFieldset) {
-		$rsvpForm.find('.rsvp-btn').attr('disabled', !(isNameFilled($guestFieldset) && isAttendanceFilled($guestFieldset) && isEntreeFilled($guestFieldset)));
+		var verifiedName = isNameFilled($guestFieldset);
+		var verifiedAttendance = isAttendanceFilled($guestFieldset);
+		var verifiedEntree = isAttending($guestFieldset) ? isEntreeFilled($guestFieldset) : true;
+
+		$rsvpForm.find('.rsvp-btn').attr('disabled', !(verifiedName && verifiedAttendance && verifiedEntree));
 	}
 
 	function isNameFilled($guestFieldset) {
@@ -60,14 +114,21 @@
 	}
 
 	function isAttendanceFilled($guestFieldset) {
-		var guest = $guestFieldset.attr('class').split(' ')[0];
+		var guest = getGuest($guestFieldset);
 
 		return !!$guestFieldset.find('[name=' + guest + '-rsvp]:checked').val();
 	}
 
+	function isAttending($guestFieldset) {
+		var guest = getGuest($guestFieldset);
+
+		return $guestFieldset.find('[name=' + guest + '-rsvp]:checked').val() === 'yes';	
+	}
+
 	function isEntreeFilled($guestFieldset) {
-		var guest = $guestFieldset.attr('class').split(' ')[0];
+		var guest = getGuest($guestFieldset);
 
 		return !!$guestFieldset.find('[name=' + guest + '-entree]:checked').val();
 	}
+
 }(jQuery));
